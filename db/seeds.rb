652e66ae1a4dd64b2a7c509b367cd159bca1a3c4
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-22 17:06:55 +0500

    Create five different greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create!(message: 'Hello Rails Backend!')
+Greeting.create!(message: 'Good morning!')
+Greeting.create!(message: 'Good day!')
+Greeting.create!(message: 'Happy Coding!')
+Greeting.create!(message: "What's up?")
